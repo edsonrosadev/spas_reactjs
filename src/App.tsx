348cd52fde185715da9179848bc1034308b2691e
edsonrosadev <edsonmarcosdarosa@gmail.com>
2024-09-11 07:42:56 -0300

    criando o projeto no git

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/Button";
+
+export function App() {
+
+  return (
+    <h1>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </h1>
+  )
+}
+
+
